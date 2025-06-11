cb1cc8f1e7a774a471e9fb9d078c6a346c7a9e51
benjamin castro <lilbenjalemos@gmail.com>
2025-06-10 20:59:01 -0400

    finalizado

--- src/main/java/com/api/reporte/ReporteApplication.java
+++ src/main/java/com/api/reporte/ReporteApplication.java
@@ -3,7 +3,7 @@ package com.api.reporte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.reporte")
 public class ReporteApplication {
 
 	public static void main(String[] args) {
